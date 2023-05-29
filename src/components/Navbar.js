c6c60e739f192260858df7303289163561df6567
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-05-29 17:38:40 +0530

    Updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="bg-red-900 py-4 px-8">
+      <div className="flex items-center justify-between">
+        <div>
+          <Link href="/">
+            <span className="text-white text-xl font-bold">Logo</span>
+          </Link>
+        </div>
+        <div className="hidden md:block">
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/login">
+                <span className="text-white">Login</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/register">
+                <span className="text-white">Register</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/manufacturer">
+                <span className="text-white">Manufacturer</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/transporter">
+                <span className="text-white">Transporter</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M18 13H6v-2h12v2zm0-5H6V6h12v2zm0 10H6v-2h12v2z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 5h16v2H4V5zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="mt-4 md:hidden">
+          <ul className="flex flex-col space-y-2">
+            <li>
+              <Link href="/login">
+                <span className="text-white">Login</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/register">
+                <span className="text-white">Register</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/manufacturer">
+                <span className="text-white">Manufacturer</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/transporter">
+                <span className="text-white">Transporter</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   const router = useRouter();
+   const { pathname } = router;
+
+   // Check if the current page is the home page
+   const isHomePage = pathname === "/";
+
+   return (
+     <div>
+       {!isHomePage && <Navbar />}
+       <Component {...pageProps} />
+     </div>
+   );
 }
